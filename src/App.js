99d123537fe3004acfe27437873c1baed0f659d4
skydweller <mt_alisangco@yahoo.com>
2019-11-16 18:08:50 -0800

    markup grid & layout

--- src/App.js
+++ src/App.js
@@ -4,21 +4,68 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <header id="pageHeader">Header
+        Women
+        Men Children Eyewear watches jewellery</header>
+        <div class="header-jumbo"> </div>
+
+        <article id="mainArticle">Article
+            <div id="inner-grid">
+                <div>1
+                    <div className="text">
+                        <h3>Seamlessly visualize quality</h3>
+                        <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                        <button>Here's why</button>
+                    </div></div>
+                <div>2     <div className="text">
+                    <h3>Seamlessly visualize quality</h3>
+                    <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                    <button>Here's why</button>
+                </div> </div>
+                <div>3     <div className="text">
+                    <h3>Seamlessly visualize quality</h3>
+                    <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                    <button>Here's why</button>
+                </div> </div>
+                <div>4
+                    <div className="text">
+                        <h3>Seamlessly visualize quality</h3>
+                        <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                        <button>Here's why</button>
+                    </div> </div>
+                <div>5
+                    <div className="text">
+                        <h3>Seamlessly visualize quality</h3>
+                        <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                        <button>Here's why</button>
+                    </div>
+                </div>
+                <div>6</div>
+                <div>7
+                    <div className="text">
+                        <h3>Seamlessly visualize quality</h3>
+                        <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                        <button>Here's why</button>
+                    </div></div>
+                <div>8</div>
+                <div>5</div>
+                <div>6
+                    <div className="text">
+                        <h3>Seamlessly visualize quality</h3>
+                        <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+                        <button>Here's why</button>
+                    </div></div>
+                <div>7</div>
+                <div>8</div>
+            </div>
+        </article>
+        <nav id="mainNav">Nav</nav>
+
+        <footer id="pageFooter">Footer
+            <div id="footer-grid">
+
+            </div> </footer>
     </div>
   );
 }
